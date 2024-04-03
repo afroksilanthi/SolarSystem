99c57373be59b1b764115b0c6a603e942a3ea252
afroksilanthi <103579857+afroksilanthi@users.noreply.github.com>
2024-04-03 10:50:24 +0300

    added controls, tried animation

--- main.js
+++ main.js
@@ -2,23 +2,34 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import gsap from 'gsap';
 
-
+//scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); 
 camera.position.set(0, 0, 5); // Set camera position to see the Earth model
 
+//renderer 
 const renderer = new THREE.WebGLRenderer(); 
+renderer.setPixelRatio(2); //smoother sphere
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
+//CONTROLS 
 const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true; //more weight
+controls.enablePan = false; //not moving around
+controls.autoRotate = true; //rotate
+controls.autoRotateSpeed = 5;
+
 controls.update();
 
+
+// Earth
 const loader = new GLTFLoader(); // Create a new loader instance
 
-loader.load('planetss/earth.glb', function ( gltf ) { 
+loader.load('files/earth.glb', function ( gltf ) { 
 
   const eObject = gltf.scene; // Get the loaded scene
   eObject.position.set(0, 0, 0); // Set x, y, z coordinates as needed
@@ -30,25 +41,46 @@ loader.load('planetss/earth.glb', function ( gltf ) {
 
 } );
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Ambient light
+
+//LIGHTS
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2); // Ambient light
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 6); // Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5); // Directional light
 directionalLight.position.set(0, 1, 0);
 scene.add(directionalLight);
 
 
 
-// Add a simple grid helper to visualize the scene
-const gridHelper = new THREE.GridHelper(100, 100);
-//scene.add(gridHelper);
-
-
+//
 function render() {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
 render();
+
+
+
+//Timeline magicc//Animation
+const t1 = gsap.timeline({defaults: { duration: 1}})
+t1.fromTo(mesh.scale, {z: 0, x: 0, y:0 }, {z: 1, x: 1, y:1 })
+// t1.fromTo({ y: "-100%" }, { y: "0%" })
+// t1.fromTo({ opacity: 0 }, { opacity: 1})
+
+
+
+//window size 
+
+window.onresize = function () {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+};
+
+
 //Background
 //const spacetexture = new THREE.TextureLoader().load('planetss/spacee.jpg');
 //scene.background = spacetexture;
