ce030293f7f1ec77891a7edba5511df22c4b4827
afroksilanthi <103579857+afroksilanthi@users.noreply.github.com>
2024-04-08 14:29:28 +0300

    added mars, better ligh, removed useless code

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 camera.position.set(-3,0, 30);
+
 // camera.lookAt( 0, 0, 0 );
 
 //Renderer
@@ -35,14 +36,15 @@ controls.enablePan = false; //not moving around
 controls.update();
 
 // Lights
+
 //not in need
 // const pointLight = new THREE.PointLight(0xffffff, 1);
 // pointLight.position.set(0, 0, 0);
 // scene.add( pointLight );
 
-// const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-// ambientLight.position.set(0, 0, 0)
-// scene.add(ambientLight);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.02);
+ambientLight.position.set(0, 0, 0)
+scene.add(ambientLight);
 
 
 /////////Earth Spotlight
@@ -58,48 +60,29 @@ spotLightE.distance = 0;
 scene.add( spotLightE );
 
 //Spotlight Venus
-const spotLightV = new THREE.SpotLight( 0xffffff, 100);
-spotLightV.position.set( -35, 0, 20);
-spotLightV.angle = Math.PI / 8;
-spotLightV.penumbra = 0.5; // Set the penumbra to soften the edges of the spotLight
-spotLightV.decay = 2;
-spotLightV.distance = 28;
+// const spotLightV = new THREE.SpotLight( 0xffffff, 100);
+// spotLightV.position.set( -35, 0, 20);
+// spotLightV.angle = Math.PI / 8;
+// spotLightV.penumbra = 0.5; // Set the penumbra to soften the edges of the spotLight
+// spotLightV.decay = 2;
+// spotLightV.distance = 28;
 
 // scene.add( spotLightV );
 
 ///Dir Light
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.6);
-directionalLight.position.set(-10, 9, 5); // Set the position of the directional light(-10, -1, 10)
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 5);
+directionalLight.position.set(-18, 58, 12); // Set position of the light(-10, -1, 10)
 scene.add(directionalLight);
 
 
 
 // SEE THE SPOTLIGHT
-const lightHelper = new THREE.SpotLightHelper( spotLightV );
-const lightHelper2 = new THREE.DirectionalLightHelper( directionalLight)
+// const lightHelper = new THREE.SpotLightHelper( spotLightV );
+// const lightHelper2 = new THREE.DirectionalLightHelper( directionalLight)
 
-scene.add( lightHelper, lightHelper2 );
+// scene.add( lightHelper, lightHelper2 );
 
-//Stars
-//
-// function addStar() {
-//   const geometry = new THREE.SphereGeometry(0.2, 3, 4, 0, 6.3, 0, 6.3);
-//   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-//   const star = new THREE.Mesh(geometry, material);
-//   star.rotation.x = 20
-//   const [x, y, z] = Array(3)
-//     .fill()
-//     .map(() => THREE.MathUtils.randFloatSpread(100));
-
-//   star.position.set(x, y, z);
-//   scene.add(star);
-// }
-
-// Array(200).fill().forEach(addStar);
-
-
-
-//EARTH
+//EARTH 3
 let earth;
 const loader = new GLTFLoader()
 loader.load( 'files/earth.glb', function(gltf){
@@ -114,26 +97,35 @@ loader.load( 'files/earth.glb', function(gltf){
   console.log(error)
 })
 
-//VENUS
+//VENUS 2
 let venus;
 loader.load( 'files/Venus.glb', function(gltf){
    venus = gltf.scene
    venus.position.set(-10, -1, 10); // Set x, y, z coordinates as needed 
    venus.scale.set(0.004, 0.004, 0.004); // Adjust the scale as needed
-
+   venus.layers.set(-1);
    scene.add( venus )
 })
 
-// Mercury
+// Mercury 1
 let mercury;
 loader.load( 'files/Mercury.glb', function(gltf){
   mercury = gltf.scene
-  mercury.position.set(-10, 0, 30); // Set x, y, z coordinates as needed 
+  mercury.position.set(-30, 0, 30); // Set x, y, z coordinates as needed 
   mercury.scale.set(0.003, 0.003, 0.003); // Adjust the scale as needed
    scene.add( mercury )
 })
 
-//SUN
+//Mars 4
+let mars;
+loader.load( 'files/Mars.glb', function(gltf){
+  mars = gltf.scene; 
+  mars.position.set(10, 0, -10); // Set x, y, z coordinates as needed
+  mars.scale.set(0.003, 0.003, 0.003); // Adjust the scale as needed
+  scene.add( mars );   // Add the scaled object to the scene
+})
+
+//SUN 
 // let sun;
 // loader.load( 'files/sun.glb', function(gltf){
 //   sun = gltf.scene
@@ -163,29 +155,32 @@ function moveCamera() {
   if(mercury){
     mercury.rotation.x += 0.05;
     mercury.rotation.y += 0.075;
-    mercury.rotation.z += 0.05;
-  }
+    mercury.rotation.z += 0.05; }
+  if(mars){
+    mars.rotation.x += 0.05;
+    mars.rotation.y += 0.085;
+    mars.rotation.z += 0.05;  }
 
-  camera.position.z = t * -0.01;//this
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
 }
 
 document.body.onscroll = moveCamera;
 moveCamera();
 
 // Animation Loop
-function animate() {
-  requestAnimationFrame(animate);
-  // torus.rotation.x += 0.01;
+// function animate() {
+//   requestAnimationFrame(animate);
+//   // torus.rotation.x += 0.01;
   // torus.rotation.y += 0.005;
   // torus.rotation.z += 0.01;
   // mercury.rotation.x += 0.005;
   // controls.update();
-  renderer.render(scene, camera);
-}
+  // renderer.render(scene, camera);
+// }
 
-animate();
+// animate();
 
 
 /////////////////////////////
@@ -200,7 +195,7 @@ const starMaterial = new THREE.MeshBasicMaterial({
 });
 // galaxy mesh
 const starMesh = new THREE.Mesh(starGeometry, starMaterial);
-starMesh.layers.set(1);
+// starMesh.layers.set(0);
 scene.add(starMesh);
 
 
@@ -213,27 +208,27 @@ const bloomPass = new UnrealBloomPass(
   0.4,
   0.85
 );
-bloomPass.threshold = 0;
-bloomPass.strength = 2; //intensity of glow
-bloomPass.radius = 0;
+bloomPass.threshold = -2; 
+bloomPass.strength = 0.5; //intensity of glow
+bloomPass.radius = 2;
 const bloomComposer = new EffectComposer(renderer);
 bloomComposer.setSize(window.innerWidth, window.innerHeight);
-bloomComposer.renderToScreen = true;
-bloomComposer.addPass(renderScene);
-bloomComposer.addPass(bloomPass);
 
 
 //sun object
 const color = new THREE.Color("#FDB813");
 const geometry = new THREE.IcosahedronGeometry(1, 15);
 const material = new THREE.MeshBasicMaterial({ color: color });
-const sphere = new THREE.Mesh(geometry, material);
-sphere.position.set(-65, 0, 30);
-sphere.scale.set(22, 22, 22);
-sphere.layers.set(1);
-scene.add(sphere);
+const Sunsphere = new THREE.Mesh(geometry, material);
+Sunsphere.position.set(-70, 0, 50);
+Sunsphere.scale.set(22, 22, 22);
+Sunsphere.layers.set(1);
+scene.add(Sunsphere);
 
+bloomComposer.render(Sunsphere);
 
+bloomComposer.addPass(renderScene);
+bloomComposer.addPass(bloomPass);
 
 
 
@@ -241,10 +236,12 @@ camera.layers.set(1);
 bloomComposer.render();
 
 starMesh.layers.set(1);
-sphere.layers.set(1);
+Sunsphere.layers.set(1);
 
 camera.layers.enableAll(); // Render all layers
 bloomComposer.render();
+
+
 
 const animatee = () => {
   requestAnimationFrame(animatee);
@@ -255,24 +252,6 @@ const animatee = () => {
 };
 
 animatee();
-
-// const params = {
-//   threshold: 0,
-//   strength: 1,
-//   radius: 0.5,
-//   exposure: 1
-// };
-
-// const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
-// bloomPass.threshold = params.threshold;
-// bloomPass.strength = params.strength;
-// bloomPass.radius = params.radius;
-
-// const bloomComposer = new EffectComposer( renderer );
-// bloomComposer.renderToScreen = false;
-// bloomComposer.addPass( renderScene );
-// bloomComposer.addPass( bloomPass );
-
 
 
 //WINDOW SIZE
