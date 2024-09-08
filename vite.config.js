ec0618b9db4bff5d200d110b48c5fd9a457a1b39
afroksilanthi <103579857+afroksilanthi@users.noreply.github.com>
2024-09-08 17:59:42 +0300

    testing mainjs in config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: './src/main.js', // Set this to your actual entry point
+    },
+  },
   base: '/SolarSystem/', 
 });
