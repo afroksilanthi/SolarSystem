a6770e0e9153972a8e49c2337ff4f7c456da65a1
afroksilanthi <103579857+afroksilanthi@users.noreply.github.com>
2024-09-08 18:08:30 +0300

    test run2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: './src/main.js', // Set this to your actual entry point
-    },
-  },
   base: '/SolarSystem/', 
 });
